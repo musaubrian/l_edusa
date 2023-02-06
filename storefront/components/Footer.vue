04778825fd5c4016c904f4493f0186b3008d018a
Ernest <musaubrian15@gmail.com>
2023-02-06 12:49:07 +0300

    chore: add footer

---
+++ storefront/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="w-full bg-gray-300 p-2">
+        <h1 class="font-semibold text-2xl text-gray-800 text-center">SomeStore</h1>
+        <div class="flex flex-col p-3">
+            <h2 class="font-semibold underline text-gray-700 text-xl">Helpful links</h2>
+            <ul>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/store">Explore store</NuxtLink></li>
+                <li><NuxtLink to="/cart">Cart</NuxtLink></li>
+            </ul>
+        </div>
+    </footer>
+</template>
